34a68a61574407a9ea3569571ec2c186f69ec21f
Andini Tribuana <threeguanan@gmail.com>
2025-04-29 22:33:18 +0700

    Jobsheet 10

---
+++ Mahasiswa02.java
@@ -0,0 +1,22 @@
+public class Mahasiswa02 {
+    String nama, nim, kelas;
+    int nilai;
+
+    Mahasiswa02(){
+
+    }
+
+    Mahasiswa02(String nama, String nim, String kelas){
+        this.nama = nama;
+        this.nim = nim;
+        this.kelas = kelas;
+        nilai = -1;
+    }
+
+    void tugasDinilai(int nilai) {
+        this.nilai = nilai;
+    }
+    public static void main (String[] args){
+        
+    }
+}
